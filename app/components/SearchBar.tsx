0c8ad455edddd5953bf954655d49759a8e77e450
psm1st <psm1st@sookmyung.ac.kr>
2026-01-01 22:40:19 +0900

    feat: search bar 구현

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export default function SearchBar() {
+  return (
+    <div className="w-full flex justify-start items-center gap-3 px-4 py-4 bg-white rounded-lg border border-green-500">
+      <div className="w-6 h-6 relative shrink-0">
+        <Image
+          src="/icons/home-icon/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+          className="w-full h-full"
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="제품을 입력하세요"
+        className="flex-1 bg-transparent outline-none text-zinc-500 text-lg font-normal font-['Pretendard'] leading-7 placeholder:text-zinc-500"
+      />
+    </div>
+  );
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import SearchBar from './components/SearchBar';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       <div><p>Hello World</p></div>
-      </main>
+    <div className="w-full max-w-[1440px] mx-auto min-h-screen px-4 py-8">
+      <SearchBar />
     </div>
   );
 }
