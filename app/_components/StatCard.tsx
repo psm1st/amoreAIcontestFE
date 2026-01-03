835d2f8822f16cbbd205ae5a967a1de6c6d72717
psm1st <psm1st@sookmyung.ac.kr>
2026-01-03 20:13:42 +0900

    desing : 길이 안맞음 문제 해결

--- app/_components/StatCard.tsx
+++ app/_components/StatCard.tsx
@@ -10,7 +10,7 @@ export default function StatCard({ label, value, icon }: StatCardProps) {
   const iconPath = `/icons/detail-icon/${icon}.svg`;
   
   return (
-    <div className="w-96 h-36 py-5 pl-9 pr-5 bg-white rounded-2xl inline-flex flex-col justify-start items-start gap-2.5">
+    <div className="w-100 h-36 py-5 pl-9 pr-5 bg-white rounded-2xl inline-flex flex-col justify-start items-start gap-2.5">
       <div className="w-80 inline-flex justify-between items-start">
         <div className="w-24 inline-flex flex-col justify-start items-start gap-5">
           <div className="self-stretch justify-start text-zinc-600 text-lg font-medium font-['Pretendard'] leading-7">
