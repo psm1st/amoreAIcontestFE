dbdd64579dfd65034538f0bbf3550a287af6766b
psm1st <psm1st@sookmyung.ac.kr>
2026-01-04 13:37:33 +0900

    feat: 외부 이미지 허용

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'down-sg.img.susercontent.com',
+      },
     ],
   },
 };
