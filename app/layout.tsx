78bd372515f173874292764cf34663d0df9f6f14
psm1st <psm1st@sookmyung.ac.kr>
2026-01-04 20:45:45 +0900

    feat: 파비콘 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "AMORE-VIEW",
   description: "Amore View",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
